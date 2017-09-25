1dcde13e96a5f4f2b987eb5b3d6ea0ce19634051
Daniel Hill <daniel@enemyplanet.geek.nz>
2017-09-26 12:59:35 +1300

    Add README

---
+++ README.mk
@@ -0,0 +1,13 @@
+### Fish Tank Automation software.
+
+#### Website
+
+Contains Website and Database Model
+
+#### EDI
+
+Contains scripts to convert PDF & Excel files to the Database
+
+#### Database
+
+Contains the Legacy Database files for MS-SQL (now on Azure)
