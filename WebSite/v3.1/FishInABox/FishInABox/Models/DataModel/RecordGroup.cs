086c76070865103f0fe481ed62fdb47626f10bb8
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-11-02 17:13:10 +1300

    generate Data Model using database design first

---
+++ WebSite/v3.1/FishInABox/FishInABox/Models/DataModel/RecordGroup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FishInABox.Models.DataModel
+{
+    public partial class RecordGroup
+    {
+        public RecordGroup()
+        {
+            RecordPet = new HashSet<RecordPet>();
+        }
+
+        public int IdPk { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<RecordPet> RecordPet { get; set; }
+    }
+}
---
+++ WebSite/v3.1/FishInABox/FishInABox/Models/DataModel/RecordPacking.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FishInABox.Models.DataModel
+{
+    public partial class RecordPacking
+    {
+        public int IdPk { get; set; }
+        public string Description { get; set; }
+    }
+}
