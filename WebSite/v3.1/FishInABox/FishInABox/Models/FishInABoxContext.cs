860388d90f9ad63b3e5d0a588cca99e5483e033e
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-11-28 11:08:26 +1300

    imported Data Model from database

---
+++ WebSite/v3.1/FishInABox/FishInABox/Models/FishInABoxContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace FishInABox.Models
+{
+    public partial class FishInABoxContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer(@"Data Source=tcp:fishinabox.database.windows.net;Initial Catalog=fishinabox;Persist Security Info=True;User ID=FIAB;Password=M0r7w1b3");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+    }
+}
